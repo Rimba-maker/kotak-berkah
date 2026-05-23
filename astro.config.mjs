aab10058a629a0d5231cbdd1ff9bc1d9729326de
Rimba-maker <caesarbimantara00@gmail.com>
2026-05-23 14:29:30 +0700

    chore: setup github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://Rimba-maker.github.io',
+  base: '/kotak-berkah',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
